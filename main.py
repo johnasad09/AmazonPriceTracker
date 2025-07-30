7e99fae523dc19b521c7e5d688c3fb0c234a8d6e
Asad Ullah <johnasad09@gmail.com>
2025-07-30 18:36:59 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
+import os
+from dotenv import load_dotenv
 import smtplib
 import requests
 from bs4 import BeautifulSoup
 
-AMAZON_URL = "URL"
+load_dotenv()
 
-MY_EMAIL = "SENDER EMAIL"
-PASSWORD = "SENDER PASSWORD"
-RECIPIENT_EMAIL = "RECIPIENT EMAIL"
+AMAZON_URL = "https://www.amazon.com/dp/B075CYMYK6?ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6&th=1"
+
+MY_EMAIL = os.environ["MY_EMAIL"]
+PASSWORD = os.environ["PASSWORD"]
+RECIPIENT_EMAIL = "abc@mail.com"
 # --------------------------------------------------------------
 def send_mail(msg):
     with smtplib.SMTP("smtp.gmail.com", 587) as connection:
@@ -15,7 +19,22 @@ def send_mail(msg):
         connection.sendmail(from_addr=MY_EMAIL, to_addrs=RECIPIENT_EMAIL, msg=msg)
 
 # -------------------------------------------------------------
-response = requests.get(AMAZON_URL)
+headers = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+    "Accept-Encoding": "gzip, deflate, br, zstd",
+    "Accept-Language": "en-GB,de;q=0.8,fr;q=0.6,en;q=0.4,ja;q=0.2",
+    "Dnt": "1",
+    "Priority": "u=1",
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "none",
+    "Sec-Fetch-User": "?1",
+    "Sec-Gpc": "1",
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:126.0) Gecko/20100101 Firefox/126.0",
+    }
+
+response = requests.get(url=AMAZON_URL, headers=headers)
 
 soup = BeautifulSoup(response.content, "html.parser")
 price = soup.find(name="span", class_="a-offscreen").get_text()
@@ -29,6 +48,7 @@ print(title)
 
 if price_without_symbol <= target_price:
     message = f"{title} is on sale for {price}!"
-    msg = f"Subject:Amazon Price Discount!\n\n{message}\n{AMAZON_URL}".encode("utf-8")
+    msg = f"Subject:Amazon Price Alert!\n\n{message}\n{AMAZON_URL}".encode("utf-8")
     send_mail(msg)
+    
 
